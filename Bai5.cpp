355a04aa80dce8affedf572b3dfc2352351331b9
nguyenduongtuan <125101917+nguyenduongtun@users.noreply.github.com>
2024-07-17 02:27:00 +0700

    Update Bai5.cpp

--- Bai5.cpp
+++ Bai5.cpp
@@ -1,15 +1,15 @@
-//#include<stdio.h>
-//#include<conio.h>
-//
-//void main()
-//{
-//	int a, b, c;
-//	printf("Nhap a: ");
-//	scanf_s("%d", &a);
-//	printf("Nhap b: ");
-//	scanf_s("%d", &b);
-//	printf("Nhap c: ");
-//	scanf_s("%d", &c);
-//	float ketqua = (float)c / (a - b);
-//	printf("ket qua:%f", ketqua);
-//}
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a, b, c;
+	printf("Nhap a: ");
+	scanf_s("%d", &a);
+	printf("Nhap b: ");
+	scanf_s("%d", &b);
+	printf("Nhap c: ");
+	scanf_s("%d", &c);
+	float ketqua = (float)c / (a - b);
+	printf("ket qua:%f", ketqua);
+}
