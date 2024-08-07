5d8dfe1461f007a6a6f66d8335e56e3f80bc7c25
nguyenduongtuan <125101917+nguyenduongtun@users.noreply.github.com>
2024-08-07 13:43:57 +0700

    Add files via upload
    up bai buoi 5

---
+++ NguyenDuongTuanBuoi5/ThapHaNoi.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+// Hàm chuyển đĩa từ cột này sang cột khác
+void MoveDisk(char fromCol, char toCol) {
+    printf("chuyen dia tu cot %c sang cot %c\n", fromCol, toCol);
+}
+
+// Hàm đệ quy giải bài toán tháp Hà Nội
+void Tower(int n, char colA, char colB, char colC) {
+    if (n == 1) {
+        MoveDisk(colA, colC);
+    }
+    else {
+        Tower(n - 1, colA, colC, colB);
+        MoveDisk(colA, colC);
+        Tower(n - 1, colB, colA, colC);
+    }
+}
+
+int main() {
+    int n;
+
+    // Nhập số đĩa từ bàn phím
+    printf("nhap so dia: ");
+    scanf_s("%d", &n);
+
+    // Gọi hàm Tower để hiển thị thứ tự chuyển các đĩa
+    Tower(n, 'A', 'B', 'C');
+
+    return 0;
+}
+
